3e3debde38297d329ac183e0299e5e6c23734b83
aslakbfj <36239161+aslakbfj@users.noreply.github.com>
2024-04-08 17:28:15 +0200

    small changes

--- bed_to_bin.sh
+++ bed_to_bin.sh
@@ -3,17 +3,22 @@
 
 
 source activate explainn
-# make bins: default 200 bin width and seq length 1000
-SALMON_FOLDER=/mnt/SCRATCH/asfj/downloads/genomes/salmon
 
+SALMON_FOLDER=/mnt/SCRATCH/asfj/downloads/genomes/salmon #where the salmon download is located
+DATA_FOLDER=/mnt/SCRATCH/asfj/AS-TAC #where you want to store the entire bed file
+HOME_DATA=/mnt/users/asfj/Thesis/data/salmon
 # make a file called "bed_list_test.txt" listing all bed file paths (one per line):
 cd $SALMON_FOLDER
+
 find AS-TAC-peaks -name "*.narrowPeak" > bed_list_test.txt
 
+
+# make bins: default 200 bin width and seq length 1000
+cd $PWD
 binify_bed.py \
   --bedlist bed_list_test.txt \
   --fasta genome/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa \
-  --outfile AS-TAC_1000bp.tsv \
+  --outfile $DATA_FOLDER/AS-TAC_1000bp.tsv \
   --seq_length 1000 \
   --exclude blacklist/AtlanticSalmon_blacklist_sorted.bed
 
@@ -32,5 +37,9 @@ binify_bed.py \
 # paste -d "\t" ./AI-TAC/AI-TAC_ids.txt ./AI-TAC/AI-TAC_sequences.txt \
 # ./AI-TAC/AI-TAC_binary_heights.txt > AI-TAC/AI-TAC_1000bp.tsv
 
+cd $DATA_FOLDER
 awk 'BEGIN {srand()} {f = FILENAME (rand() <= 0.1 ? ".validation" : ".train");
 print > f}' ./AS-TAC_1000bp.tsv
+
+mv ./AS-TAC_1000bp.tsv.train $HOME_DATA/AS-TAC_1000bp.train.tsv
+mv  ./AS-TAC_1000bp.tsv.validation $HOME_DATA/AS-TAC_1000bp.validation.tsv
