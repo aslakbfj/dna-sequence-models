2ff8802c0c27d93cc485050efd9570eb081f5e6f
aslakbfj <36239161+aslakbfj@users.noreply.github.com>
2024-05-08 08:53:14 +0200

    remove bad target labels

--- bed_to_bin.sh
+++ bed_to_bin.sh
@@ -8,7 +8,7 @@ echo "environment activated"
 
 script_dir=$PWD #path of binify_bed.py 
 SALMON_FOLDER=/mnt/SCRATCH/asfj/downloads/genomes/salmon #where the salmon download is located
-DATA_FOLDER=/mnt/SCRATCH/asfj/AS-TAC #where you want to store the entire bed file
+DATA_FOLDER=/mnt/SCRATCH/asfj/AS-TAC #where you want to store the tsv files
 HOME_DATA=/mnt/users/asfj/Thesis/data/salmon #where you want to store the training and validation files
 
 # make and if statement to check if the file exists
@@ -41,19 +41,48 @@ else
     mv  ./AS-TAC_1000bp.tsv.validation $HOME_DATA/AS-TAC_1000bp.validation.tsv
 fi
 
-if [ -f $HOME_DATA/AS-TAC_1000bp_no21_no25.tsv ]; then
+if [ -f $DATA_FOLDER/AS-TAC_21_25_test.tsv ]; then
   echo "21_25.tsv exists"
 else
     # make a subset of AS-TAC_1000bp.tsv of lines where the first column starts with "21" or "25"
     echo "creating subset of AS-TAC_1000bp.tsv"
-    awk '$1 ~ /^21|^25/' $DATA_FOLDER/AS-TAC_1000bp.tsv > $HOME_DATA/AS-TAC_1000bp_21_25.tsv
+    awk '$1 ~ /^21|^25/' $DATA_FOLDER/AS-TAC_1000bp.tsv > $DATA_FOLDER/AS-TAC_21_25_test.tsv
     # make a subset of the data that did not contain 21 or 25
     echo "creating subset of AS-TAC_1000bp.tsv"
-    awk '$1 !~ /^21|^25/' $DATA_FOLDER/AS-TAC_1000bp.tsv > $HOME_DATA/AS-TAC_1000bp_no21_no25.tsv
+    awk '$1 !~ /^21|^25/' $DATA_FOLDER/AS-TAC_1000bp.tsv > $DATA_FOLDER/AS-TAC_21_25_train.tsv
 fi
 
-echo "done"
 
-#just testing
-date -d @${SLURM_SUBMIT_TIMESTAMP}
-echo $date
+# If $DATA_FOLDER/AS-TAC_noMuscle_1000bp.tsv does not exist, do the next
+if [ -f $DATA_FOLDER/AS-TAC_noMuscle_1000bp.tsv ]; then
+  echo "AS-TAC_noMuscle_1000bp.tsv exists"
+else
+  # Make other subsets of the data, where we remove classes 
+  echo "creating subset of AS-TAC_1000bp.tsv"
+  cd $SALMON_FOLDER
+  # remove all lines that contain "Muscle", "Gonad_Immature_Female_R2" or "Gonad_Mature_Male_R2" from bed_list_full.txt, and remove the corresponding columns from each line of AS-TAC_1000bp.tsv
+  awk '$0 !~ /Muscle/' bed_list_test.txt \
+  | awk '$0 !~ /Gonad_Immature_Female_R2/' \
+  | awk '$0 !~ /Gonad_Mature_Male_R2/' > bed_list_noMuscle.txt
+
+  # Make the new tsv with the new bed_list
+  python $script_dir/binify_bed.py \
+    --bedlist bed_list_noMuscle.txt \
+    --fasta genome/Salmo_salar.Ssal_v3.1.dna_sm.toplevel.fa \
+    --outfile $DATA_FOLDER/AS-TAC_noMuscle_1000bp.tsv \
+    --seq_length 1000 \
+    --exclude blacklist/AtlanticSalmon_blacklist_sorted.bed
+fi
+
+# If $DATA_FOLDER/AS-TAC_noMuscle_21_25_test.tsv does not exist, do the next
+if [ -f $DATA_FOLDER/AS-TAC_noMuscle_21_25_test.tsv ]; then
+  echo "AS-TAC_noMuscle_21_25_test.tsv exists"
+else
+  # make a subset of AS-TAC_noMuscle_1000bp.tsv of lines where the first column starts with "21" or "25"
+  echo "creating subset of AS-TAC_noMuscle_1000bp.tsv"
+  awk '$1 ~ /^21|^25/' $DATA_FOLDER/AS-TAC_noMuscle_1000bp.tsv > $DATA_FOLDER/AS-TAC_noMuscle_21_25_test.tsv
+  # make a subset of the data that did not contain 21 or 25
+  echo "creating subset of AS-TAC_noMuscle_1000bp.tsv"
+  awk '$1 !~ /^21|^25/' $DATA_FOLDER/AS-TAC_noMuscle_1000bp.tsv > $DATA_FOLDER/AS-TAC_noMuscle_21_25_train.tsv
+fi
+
